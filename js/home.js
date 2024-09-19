d0a879afd46888bd0309a5eb8dd4a4e05b3d3ecb
KI-Anik <iamkhairul101@gmail.com>
2024-09-19 15:58:35 +0600

    added partially the add money event

--- js/home.js
+++ js/home.js
@@ -1,1 +1,13 @@
-console.log('object')
+//  add money to the account
+
+document.getElementById('btn-add-money').addEventListener('click', function(event){
+    event.preventDefault();
+
+    // step 2: get new added money
+    const addMoneyInput = document.getElementById('input-add-money').value;
+    console.log(addMoneyInput)
+
+    //get the pin provided
+    const pinNumberInput = document.getElementById('input-pin-number').value;
+    console.log(pinNumberInput)
+})
